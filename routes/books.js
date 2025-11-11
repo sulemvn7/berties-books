121d224636a2bff6fd4a91ea8e5049614760d668
Suleman <149071013+sulemvn7@users.noreply.github.com>
2025-11-11 18:08:43 +0000

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -1,56 +1,22 @@
 // Create a new router
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-//Search Route
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
+// Search Route
+router.get('/search', function(req, res, next){
+    res.render("search.ejs");
 });
 
 router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    res.send("You searched for: " + req.query.keyword);
 });
 
-//Book Display Route
+// Book Display Route
 router.get('/list', function(req, res, next) {
-        let sqlquery = "SELECT * FROM books"; // query database to get all the books
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                next(err)
-            }
-            res.render("list.ejs", { availableBooks: result })
-         });
-    });
-
-// Display the Add Book form
-router.get('/addbook', function(req, res, next) {
-    res.render('addbook.ejs');
-});
-
-// Create a new router
-const express = require("express")
-const router = express.Router()
-
-//Search Route
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
-});
-
-router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
-});
-
-//Book Display Route
-router.get('/list', function(req, res, next) {
-    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    let sqlquery = "SELECT * FROM books"; 
     db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.render("list.ejs", { availableBooks: result })
+        if (err) return next(err);
+        res.render("list.ejs", { availableBooks: result });
     });
 });
 
@@ -65,17 +31,11 @@ router.post('/bookadded', function (req, res, next) {
     let newrecord = [req.body.name, req.body.price];
 
     db.query(sqlquery, newrecord, (err, result) => {
-        if (err) {
-            next(err);
-        } else {
-            res.send('✅ This book has been added to the database.<br>Name: ' 
-                     + req.body.name + '<br>Price: £' + Number(req.body.price).toFixed(2));
-        }
+        if (err) return next(err);
+        res.send('✅ This book has been added to the database.<br>Name: ' 
+                 + req.body.name + '<br>Price: £' + Number(req.body.price).toFixed(2));
     });
 });
 
-// Export the router object so index.js can access it
-module.exports = router
-
-
-
+// Export the router
+module.exports = router;
