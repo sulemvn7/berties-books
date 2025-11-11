eff2a34f729ed5dad74c28167bbf00a78096de7a
Suleman <149071013+sulemvn7@users.noreply.github.com>
2025-11-11 17:55:40 +0000

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = express.Router()
 
+//Search Route
 router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
@@ -23,6 +24,61 @@ router.get('/list', function(req, res, next) {
          });
     });
 
+// Display the Add Book form
+router.get('/addbook', function(req, res, next) {
+    res.render('addbook.ejs');
+});
+
+// Create a new router
+const express = require("express")
+const router = express.Router()
+
+//Search Route
+router.get('/search',function(req, res, next){
+    res.render("search.ejs")
+});
+
+router.get('/search-result', function (req, res, next) {
+    //searching in the database
+    res.send("You searched for: " + req.query.keyword)
+});
+
+//Book Display Route
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("list.ejs", { availableBooks: result })
+    });
+});
+
+// Display the Add Book form
+router.get('/addbook', function(req, res, next) {
+    res.render('addbook.ejs');
+});
+
+// POST route to save the new book
+router.post('/bookadded', function (req, res, next) {
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+    let newrecord = [req.body.name, req.body.price];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.send('✅ This book has been added to the database.<br>Name: ' 
+                     + req.body.name + '<br>Price: £' + Number(req.body.price).toFixed(2));
+        }
+    });
+});
+
 // Export the router object so index.js can access it
 module.exports = router
+
+
+// Export the router object so index.js can access it
+module.exports = router
+
 
